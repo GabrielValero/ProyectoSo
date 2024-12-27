61ba5b94f9bc84ba533df915264e8a42ce57f569
Andry1902 <andreinacarolaineve@gmail.com>
2024-12-26 20:08:25 -0400

    Cabeceras de Visitar y Visitado
    Archivos .h para la compilacion

---
+++ Visitado.h
@@ -0,0 +1,8 @@
+// Visitado.h
+#ifndef VISITADO_H
+#define VISITADO_H
+
+void insertarVisitado(char *x);
+char* extraerVisitado();
+
+#endif
---
+++ Visitar.h
@@ -0,0 +1,8 @@
+// Visitar.h
+#ifndef VISITAR_H
+#define VISITAR_H
+
+void insertarVistitar(char *x);
+char* extraerVisitar();
+
+#endif

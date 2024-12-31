9915e481aaa895397a238f58fbc0c148dedb95ec
Gabriel <gabriel.131280@gmail.com>
2024-12-31 21:34:56 +0000

    Se creo la funcion para escanear el directorio

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include "Cola.h"
+
+
+void scanDirectory(char *route){
+    //abre la direccion
+    DIR * dir = opendir(route);
+    //valida que se haya abierto bien
+    if(dir == NULL){
+        perror("No se pudo abrir el directorio");
+        return;
+    }
+
+    struct dirent *element;
+    struct stat info;
+    char completeDir[1024];
+    //Revisa que lea todos los elementos
+    while((element = readdir(dir)) != NULL){
+        //saca los archivos . y ..
+        if(strcmp(element->d_name, ".") == 0 || strcmp(element->d_name, "..") == 0){
+            continue;
+        }
+        //Ajusta la direccion completa del elemento
+        snprintf(completeDir, sizeof(completeDir), "%s/%s", route, element->d_name);
+        //revisa si es un directorio o un archivo
+        if(stat(completeDir, &info) == 0){
+            //si es directorio que haga la recursividad
+            if(S_ISDIR(info.st_mode)){
+                printf("Directorio: %s \n", completeDir);
+                scanDirectory(completeDir);
+            }else if(S_ISREG(info.st_mode)){
+                //Sino ps que solo imprima la direccion, aqui va el uso de la cola
+                printf("Archivo: %s \n", completeDir);
+            }
+        }
+    }
+    closedir(dir);
+};
+
+int main(int argc, char *argv[]){
+    
+    scanDirectory(argv[1]);
+    
+    return 0;
+}

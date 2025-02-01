6265d84353ec10d376bb9fcefb30a3ee3ca06204
Andry1902 <andreinacarolaineve@gmail.com>
2025-01-31 23:09:04 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
     findDuplicates(files, fileCount, &scannedList);
     
     freeQueue(&scanList);
-    freeQueue(&scanneedList);
+    freeQueue(&scannedList);
     sem_destroy(&semMax);
     sem_destroy(&semCount);
     return 0;
